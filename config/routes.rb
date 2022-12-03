63ded753dba9d8bb13c18610f8e2a9b1b2cee371
Hayden <contact@winisreallybored.com>
2022-12-02 17:32:28 -0800

    Saturn routes - update them!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'saturn/login'
   get 'saturn/logout'
   get 'saturn/panel'
-  get 'saturn/root'
+  get 'saturn', to: 'saturn#root'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
